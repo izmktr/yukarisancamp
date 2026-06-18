b1cada4207f1295177c22707dda7d1d68f95fba8
izmktr <izmktr@gmail.com>
2026-06-18 22:25:47 +0900

    feat: dotenvを追加し、環境変数の設定を行う

--- web-app-ts/dist/index.js
+++ web-app-ts/dist/index.js
@@ -3,6 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ path: '.env.local' });
 const express_1 = __importDefault(require("express"));
 const express_session_1 = __importDefault(require("express-session"));
 const path_1 = __importDefault(require("path"));
--- web-app-ts/src/index.ts
+++ web-app-ts/src/index.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
+
 import express from 'express';
 import session from 'express-session';
 import path from 'path';
