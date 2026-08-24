4298123a61ea12c8a8a89c0a7d4e5a89ea9e4843
izmktr <izmktr@gmail.com>
2026-08-24 16:16:03 +0900

    Remove generated TypeScript definition and source map files from the dist directory

--- web-app-ts/dist/routes/board.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'express-session' {
-    interface SessionData {
-        editingArticle?: any;
-    }
-}
-declare const router: import("express-serve-static-core").Router;
-export declare function refreshBoardCharaImageCache(): number;
-export default router;
-//# sourceMappingURL=board.d.ts.map
